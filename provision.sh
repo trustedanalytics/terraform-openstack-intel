c5a34dd85c72447f998f7cae94c3079f9c54e110
mszumila <marcin.szumilak@intel.com>
2016-01-05 15:46:36 +0100

    preparing .gpg structure to avoid 'options are not yet active during this run'

--- provision.sh
+++ provision.sh
@@ -142,6 +142,7 @@ cd $HOME
 
 if [[ ! -d "$HOME/.rvm" ]]; then
   cd $HOME
+  gpg --list-keys
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
   curl -sSL https://get.rvm.io | bash -s stable
 fi
